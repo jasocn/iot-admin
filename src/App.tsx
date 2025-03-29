23a9c2dfe28a7e6b03d415c48fce29c08860d1f3
wenjie meng <wenjie.meng@siemens.com>
2025-03-30 01:17:34 +0800

    improve

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 import './App.css'
-import { Button } from "./components/ui/button"
 
 function App() {
   return (
     <div className="flex flex-col items-center justify-center min-h-svh">
-      <Button>Click me</Button>
+     
     </div>
   )
 }
